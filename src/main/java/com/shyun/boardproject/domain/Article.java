10f577f60f4af9d92cbfa025a12db5b592ffa299
shyun <tjdgh0312@naver.com>
2024-11-30 09:33:39 +0900

    auditing fields

--- src/main/java/com/shyun/boardproject/domain/Article.java
+++ src/main/java/com/shyun/boardproject/domain/Article.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
@@ -22,7 +23,7 @@ import java.util.Set;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Article {
+public class Article extends AuditingFields{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -40,18 +41,6 @@ public class Article {
     @ToString.Exclude
     private final Set<ArticleComment> articleCommentSet = new LinkedHashSet<>();
 
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt; //생성일시
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy; //생성자
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt; //수정일시
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy; //수정자
 
     public Article(String title, String content, String hashtag) {
         this.title = title;
--- src/main/java/com/shyun/boardproject/domain/ArticleComment.java
+++ src/main/java/com/shyun/boardproject/domain/ArticleComment.java
@@ -6,6 +6,8 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -19,7 +21,7 @@ import java.util.Objects;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class ArticleComment {
+public class ArticleComment extends AuditingFields{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -29,19 +31,6 @@ public class ArticleComment {
     @Setter
     @Column(nullable = false, length = 500)
     private String content; //본문
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt; //생성일시
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy; //생성자
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt; //수정일시
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy; //수정자
 
 
     private ArticleComment(Article article, String content) {
