837e3f67d1a42877e4e03499077f45d782fcea23
shyun <tjdgh0312@naver.com>
2024-12-01 14:25:43 +0900

    인덱스페이지

--- src/test/java/com/shyun/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/shyun/boardproject/controller/ArticleControllerTest.java
@@ -1,5 +1,6 @@
 package com.shyun.boardproject.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @DisplayName("View  컨트롤러 - 게시글")
@@ -22,7 +22,8 @@ class ArticleControllerTest {
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
-    @DisplayName("[view][GET] 게시글 리스트 - 정상 호출 ")
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) - 정상 호출 ")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
         //given
@@ -30,10 +31,11 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
     }
-
+    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세페이지 - 정상 호출 ")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnArticlesView() throws Exception {
@@ -42,10 +44,11 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search-hashtag"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("article"));
     }
-
+    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출 ")
     @Test
     public void givenNothing_whenRequestingArticleSearchView_thenReturnArticlesDetailView() throws Exception {
@@ -54,8 +57,9 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
+    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출 ")
     @Test
     public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnArticlesDetailView() throws Exception {
@@ -64,6 +68,6 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
 }
