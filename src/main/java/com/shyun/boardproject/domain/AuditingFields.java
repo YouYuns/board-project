7008f3cc66719f33ec1e5bb55ef7871915bb33a5
shyun <tjdgh0312@naver.com>
2024-12-12 12:55:13 +0900

    auditingFields 수정

--- src/main/java/com/shyun/boardproject/domain/AuditingFields.java
+++ src/main/java/com/shyun/boardproject/domain/AuditingFields.java
@@ -18,22 +18,25 @@ import java.time.LocalDateTime;
 @ToString
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-public class AuditingFields {
+//추상클래스 변경
+//회원 도메인에서 인증이 없는 상태의 회원정보를 저장하기 위해서 createdBy, modifiedBy를 엔티티가 직접 작성해야되는
+//요구사항이 생길것으므로 이것이 가능하게 접근제어자 변경
+public abstract class AuditingFields {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt; //생성일시
+    protected LocalDateTime createdAt; //생성일시
 
     @CreatedBy
     @Column(nullable = false, length = 100, updatable = false)
-    private String createdBy; //생성자
+    protected String createdBy; //생성자
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @LastModifiedDate
     @Column(nullable = false)
-    private LocalDateTime modifiedAt; //수정일시
+    protected LocalDateTime modifiedAt; //수정일시
 
     @LastModifiedBy
     @Column(nullable = false, length = 100)
-    private String modifiedBy; //수정자
+    protected String modifiedBy; //수정자
 }
